81df2d5bdc2943d4495362c67ed293092bc3543e
top <top180802@gmail.com>
2024-03-18 14:12:57 +0700

    add prisma

---
+++ src/app/api/ticket/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { createTicket } from "../../../../prisma";
+
+export const POST = async (req: Request) => {
+  try {
+    const data = await req.text();
+    // const body = JSON.parse(data);
+    const ticket = await createTicket();
+
+    if (!ticket) {
+      return NextResponse.json(
+        { message: "Lỗi" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ ticket }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ message: "Có lỗi xảy ra" }, { status: 500 });
+  }
+};
+
+// export const GET = async () => {
+//   try {
+//     const settings = await getSettings();
+
+//     return NextResponse.json({ settings }, { status: 200 });
+//   } catch (e) {
+//     return NextResponse.json({ message: "Có lỗi xảy ra" }, { status: 500 });
+//   }
+// };
